6f47c33366c14bf631e93f7d83033697f90f4543
Nemanja <ladro_magro@yahoo.com>
2023-06-12 14:15:52 -0400

    initial commit

---
+++ Spring-Security-with-Database/src/main/java/com/school/SpringSecuritywithDatabase/model/AdminController.java
@@ -0,0 +1,25 @@
+package com.school.SpringSecuritywithDatabase.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/secure/auth")
+public class AdminController {
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+    @Autowired
+    private UserRepository userRepository;
+    @PostMapping("/admin/add")
+    public String addUser(@RequestBody User user){
+        String pwd = user.getPassword();
+        String encryptedPwd = passwordEncoder.encode(pwd);
+        user.setPassword(encryptedPwd);
+        userRepository.save(user);
+        return "User added";
+    }
+}
---
+++ Spring-Security-with-Database/src/main/java/com/school/SpringSecuritywithDatabase/model/ApplicationController.java
@@ -0,0 +1,14 @@
+package com.school.SpringSecuritywithDatabase.model;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/auth")
+public class ApplicationController {
+    @GetMapping("/process")
+    public String process(){
+        return "passed the spring security through DB";
+    }
+}
